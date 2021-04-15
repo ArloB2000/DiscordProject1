b0dd451ba80da26e7ad7ede690ba9e189174ec8f
Arlo <74018574+ArloB2000@users.noreply.github.com>
2021-04-15 13:17:32 -0400

    Create Index.js

---
+++ Bot/Index.js
@@ -0,0 +1,4 @@
+//start development soon
+//discord bot script that is a game bot
+// \(-.-)/
+console.log("This bot is currently in development or offline, please do not download the bot as it will not function. If you have downloaded this, please delete it as it has no function.");
